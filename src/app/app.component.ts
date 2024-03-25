f1919840d236af123aab4dfe5957446fcac25e46
davidsan <david.sanluis@qrsof.com>
2024-03-25 08:47:36 -0600

    detect paths

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import {Deeplinks} from "@awesome-cordova-plugins/deeplinks/ngx";
+import {Platform} from "@ionic/angular";
+import { StatusBar, Style } from '@capacitor/status-bar';
+import {alert} from "ionicons/icons";
 
 @Component({
   selector: 'app-root',
@@ -7,5 +10,33 @@ import {Deeplinks} from "@awesome-cordova-plugins/deeplinks/ngx";
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private deepLinks: Deeplinks) {}
+
+  isAlertOpen = false;
+  alertButtons = ['Action'];
+  message = 'deep link message :D'
+
+  constructor(
+    private platform: Platform,
+    private deepLinks: Deeplinks
+  ) {
+    this.platform.ready().then(() => {
+      console.log("platform ready")
+      this.deepLinks.route({
+        "/" : {}
+      }).subscribe((match) => {
+        this.message = JSON.stringify(match)
+        this.setOpen(true)
+        console.log("match: ", match)
+      },(noMatch)=> {
+        this.message = JSON.stringify(noMatch)
+        this.setOpen(true)
+        console.log("no match: ", noMatch)
+      })
+    })
+  }
+
+
+  setOpen(isOpen: boolean) {
+    this.isAlertOpen = isOpen;
+  }
 }
